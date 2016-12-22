5fca1a59a81812c488ab6cba8c7f5d9d660abe23
Michal Niewrzal <michal.n@zend.com>
2016-12-22 10:31:14 +0100

    Add support for TCP connection on Windows

--- org.eclipse.lsp4e.php/src/org/eclipse/lsp4e/php/PHPLanguageServer.java
+++ org.eclipse.lsp4e.php/src/org/eclipse/lsp4e/php/PHPLanguageServer.java
@@ -11,33 +11,63 @@
 package org.eclipse.lsp4e.php;
 
 import java.io.IOException;
-import java.net.URL;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.eclipse.core.runtime.FileLocator;
-import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.lsp4e.LanguageServerPlugin;
 import org.eclipse.lsp4e.ProcessStreamConnectionProvider;
+import org.eclipse.lsp4e.StreamConnectionProvider;
 import org.osgi.framework.Bundle;
 
-public class PHPLanguageServer extends ProcessStreamConnectionProvider {
+@SuppressWarnings("restriction")
+public class PHPLanguageServer implements StreamConnectionProvider {
+
+	private static final int CONNECTION_PORT = 29543;
+
+	private StreamConnectionProvider provider;
 
 	public PHPLanguageServer() {
 		List<String> commands = new ArrayList<>();
 		commands.add("php");
-
 		Bundle bundle = Activator.getContext().getBundle();
+		Path workingDir = Path.EMPTY;
 		try {
-			Path path = new Path(FileLocator.toFileURL(FileLocator.find(bundle, new Path("server"), null)).getPath());
-			commands.add(path.append("/bin/php-language-server.php").toOSString());
-			setCommands(commands);
-			setWorkingDirectory(path.toOSString());
+			workingDir = new Path(FileLocator.toFileURL(FileLocator.find(bundle, new Path("server"), null)).getPath());
+			commands.add(workingDir.append("/bin/php-language-server.php").toOSString());
 		} catch (IOException e) {
 			LanguageServerPlugin.logError(e);
 		}
+		if (!Platform.getOS().equals(Platform.OS_WIN32)) {
+			commands.add("--tcp=127.0.0.1:" + CONNECTION_PORT);
+			provider = new SocketStreamConnectionProvider(commands, workingDir.toOSString(), CONNECTION_PORT);
+		} else {
+			provider = new ProcessStreamConnectionProvider(commands, workingDir.toOSString());
+		}
+	}
+
+	@Override
+	public void start() throws IOException {
+		provider.start();
+	}
+
+	@Override
+	public InputStream getInputStream() {
+		return provider.getInputStream();
+	}
+
+	@Override
+	public OutputStream getOutputStream() {
+		return provider.getOutputStream();
+	}
+
+	@Override
+	public void stop() {
+		provider.stop();
 	}
 
 }
